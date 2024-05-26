bafe7c7455342ce12ee1a4ade17fdd14cc5e7b3b
heescodes <105621606+heescodes@users.noreply.github.com>
2024-05-26 20:16:56 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240526/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240526/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -6,6 +6,32 @@ START = 0
 END = 0
 seqcnt = 1
 
+def cal_seqcnt(start, t):
+    global seqcnt, END
+    tmpcnt = 1
+    for i in range(start, len(arr)-1):
+        if arr[i+1] > t:
+            tmpcnt += 1
+            END = i
+            #print(f'tmpcnt: {tmpcnt}')
+        else:
+            END = i
+            break
+        #print(f'i: {i}')
+    seqcnt = max(tmpcnt, seqcnt)
+
+idx = 0
+while(idx < len(arr)-1):
+    #print(f'idx: {idx}')
+    if arr[idx] > t:
+        cal_seqcnt(idx, t)
+        idx = END + 1
+    else:
+        idx += 1
+    #print(f'idx: {idx}')
+    #print('------------')
+
+'''
 def cal_seqcnt(t):
     global seqcnt, START, END
     tmpcnt = 1
@@ -33,36 +59,7 @@ while(idx < len(arr)-1):
     else:
         idx += 1
     #print('------------')
-
 '''
-def cal_seqcnt(start):
-    global seqcnt, START, END
-    tmpcnt = 1
-    for i in range(START, len(arr)-1):
-        END = i+1
-        if arr[i]+1 == arr[i+1]:
-            tmpcnt += 1
-            #print(f'tmpcnt: {tmpcnt}')
-        else:
-            break
-            #print(f'i: {i}')
-    seqcnt = max(tmpcnt, seqcnt)
-    
-
-idx = 0
-while(idx < len(arr)-1):
-    #print(f'idx:{idx}')
-    if arr[idx] > t:
-        START = idx
-        #print(f'START:{START}')
-        cal_seqcnt(START)
-        #print(f'END:{END}')
-        idx = END
-    else:
-        idx += 1
-    #print('------------')
-'''
-
 
 if seqcnt == 1:
     print(0)
