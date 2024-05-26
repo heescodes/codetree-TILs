09bb382ee05f9292aee08b1374cbd996efe10ed2
heescodes <105621606+heescodes@users.noreply.github.com>
2024-05-26 19:20:42 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240526/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -0,0 +1,42 @@
+#17:37
+
+n, t = map(int,input().split())
+arr = [int(x) for x in input().split()]
+START = 0
+END = 0
+seqcnt = 1
+
+#print(f'len(arr): %d',len(arr))
+
+def cal_seqcnt(start):
+    global seqcnt, START, END
+    tmpcnt = 1
+    for i in range(START, len(arr)-1):
+        END = i+1
+        if arr[i]+1 == arr[i+1]:
+            tmpcnt += 1
+            #print(f'tmpcnt: {tmpcnt}')
+        else:
+            break
+            #print(f'i: {i}')
+            
+    seqcnt = max(tmpcnt, seqcnt)
+    
+
+idx = 0
+while(idx < len(arr)-1):
+    #print(f'idx:{idx}')
+    if arr[idx] > t:
+        START = idx
+        #print(f'START:{START}')
+        cal_seqcnt(START)
+        #print(f'END:{END}')
+        idx = END
+    else:
+        idx += 1
+    #print('------------')
+
+if seqcnt == 1:
+    print(0)
+elif seqcnt > 1:
+    print(seqcnt)
