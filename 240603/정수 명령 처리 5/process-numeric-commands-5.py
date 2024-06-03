e6262f120cd81d0beb29c63355d713aa26657f70
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-03 20:28:01 +0900

    Title: 정수 명령 처리 5, Time: 244ms, Memory: 28MB, Status: Passed - CodeTree

--- 240603/정수 명령 처리 5/process-numeric-commands-5.py
+++ 240603/정수 명령 처리 5/process-numeric-commands-5.py
@@ -1,1 +1,15 @@
-w4rtw4r
+n = int(input())
+vector = []
+try:
+    while 1:
+        command = input()
+        if 'push_back' in command:
+            vector.append(int(command.split()[1]))
+        elif 'pop_back' in command:
+            vector.pop()
+        elif 'size' in command:
+            print(len(vector))
+        elif 'get' in command:
+            print(vector[int(command.split()[1])-1])
+except:
+    pass
