317516d14a505b1ef874a150262d6c25974df705
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-06 22:21:27 +0900

    Title: 특정 수와 근접한 합, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240606/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240606/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,2 +1,12 @@
-N, S = map(int,input())
-print(N)
+N, S = map(int,input().split())
+arr = [int(x) for x in input().split()]
+min_gap = 10000
+
+for i in range(N):
+    for j in range(i+1, N):
+        cur_sum = sum(arr) - arr[i] - arr[j]
+        cur_gap = abs(S - cur_sum)
+
+        min_gap = min(cur_gap, min_gap)
+
+print(min_gap)
