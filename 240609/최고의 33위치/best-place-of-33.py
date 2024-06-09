16fb7a04be67a8d2fe53fba2a0bae6f3efb82d3b
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-09 14:48:21 +0900

    Title: 최고의 33위치, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240609/최고의 33위치/best-place-of-33.py
+++ 240609/최고의 33위치/best-place-of-33.py
@@ -4,12 +4,12 @@ arr = []
 coins = 0
 for _ in range(N):
     arr.append([int(nums) for nums in input().split()])
+#arr = [list(map(int, input.split())) for _ in range(n)]
 
 def scan_3x3(y,x):
     res = 0
     for a in range(y,y+3):
         for b in range(x, x+3):
-            #print("arr[%d][%d]: %d" % (a,b,arr[a][b]))
             if arr[a][b] == 1:
                 res += 1
     return res
@@ -17,6 +17,5 @@ def scan_3x3(y,x):
 for i in range(N-2):
     for j in range(N-2):
         cmp_coin = scan_3x3(i,j)
-        #print(f'cmp_coin:{cmp_coin}')
         coins = max(coins, cmp_coin)
 print(coins)
