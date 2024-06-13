52b8c60966ef76c86f3e0b1a6ef6b486bfe36702
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-14 01:50:32 +0900

    Title: 양수 직사각형의 최대 크기, Time: 211ms, Memory: 26MB, Status: Passed - CodeTree

--- 240614/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240614/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -8,22 +8,28 @@ res = 0
 
 def is_in_range(x,y,w,l):
     if (0 <= x <= m-w) and (0 <= y <= n-l):
+        #print("range true")
         return True
     else:
+        #print("range false")
         return False
 
 def is_valid_squ(x,y,w,l):
-    for i in range(x,x+w):
-        for j in range(y,y+l):
+    for j in range(y,y+l):
+        for i in range(x,x+w):
             if arr[j][i] <= 0:
+                #print("square false")
                 return False
+    #print("square true")
     return True
 
-for r in range(n):
-    for c in range(m):
-        for w in range(1,m-r+1):
-            for l in range(1,n-c+1):
-                if is_in_range(r,c,w,l) and is_valid_squ(r,c,w,l):
+for r in range(n): #y
+    for c in range(m): #x
+        for l in range(1,n-r+1): #y
+            for w in range(1,m-c+1): #x
+                #print(f'r,c,w,l : {r},{c},{w},{l}')
+                #print("----------------")
+                if is_in_range(c,r,w,l) and is_valid_squ(c,r,w,l):
                     res = max(res, w*l)
 
 if res == 0:
