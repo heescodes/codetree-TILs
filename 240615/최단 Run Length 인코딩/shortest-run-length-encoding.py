74d24983754add1cc587ce4220c84d9b3cbb76a2
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-15 17:26:03 +0900

    Title: 최단 Run Length 인코딩, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240615/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,45 @@
+#16:23
+
+strs = list(input())
+
+#종료조건
+#그냥 매번RLE체크
+
+#rshft
+def rshft():
+    strs.insert(0,strs.pop())
+
+#get RLE
+def get_RLE():
+    res = list()
+    cnt = 0
+    pre_key = strs[0]
+    #print(f'strs:{strs}')
+    for i in range(len(strs)):
+        if i == len(strs)-1:
+            res.append(pre_key)
+            res.append(cnt)
+                
+        cur_key = strs[i]
+        if pre_key != cur_key:
+            res.append(pre_key)
+            res.append(cnt)
+            cnt = 1
+        else:    
+            cnt += 1
+
+        pre_key = cur_key 
+
+    return res
+
+#main
+rle = list()
+res_len = 1000
+
+for _ in range(len(strs)):
+    rle = get_RLE()
+    #print(rle)
+    res_len = min(len(rle),res_len)
+    rshft()
+
+print(res_len)
