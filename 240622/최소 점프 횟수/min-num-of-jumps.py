996732660a84de2ddad26ae8132d3dfb5f26ef41
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-22 18:24:13 +0900

    Title: 최소 점프 횟수, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240622/최소 점프 횟수/min-num-of-jumps.py
+++ 240622/최소 점프 횟수/min-num-of-jumps.py
@@ -1,7 +1,7 @@
 n = int(input())
 arr = list(int(nums) for nums in input().split())
 
-result = n
+result = n+1
 
 
 # idx : 0~n-1
@@ -11,12 +11,14 @@ def jump(idx, jcnt):
     #exit condition
     if idx == n-1:
         result = min(result, jcnt)
+        #print(f'---idx,jcnt,result = {idx},{jcnt},{result}')
         return 0
     elif idx > n-1 or arr[idx] == 0:
         return -1    
 
     #normal
     for mov in range(1, arr[idx]+1):
+        #print(f'idx,jcnt,result = {idx},{jcnt},{result}')
         idx += mov
         jcnt += 1
         jump(idx, jcnt)
@@ -25,7 +27,8 @@ def jump(idx, jcnt):
 
     return -1
 
-if jump(0,0) < 0:
+jump(0,0)
+if result > n+1:
     print(-1)
 else:
     print(result)
