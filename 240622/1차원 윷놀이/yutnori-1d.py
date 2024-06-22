8f0dbfdd75c2056401b5b5c6a0e1a68539f23772
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-22 17:13:57 +0900

    Title: 1차원 윷놀이, Time: 495ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240622/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,37 @@
+n,m,k = map(int,input().split())
+distances = list(int(nums) for nums in input().split())
+positions = [1]*(k+1)
+result = 0
+
+
+def get_score():
+    global result
+    score = 0
+    for pos in positions:
+        if pos >= m:
+            score += 1
+    return score
+
+def choose(cnt):
+    global result
+
+    result = max(result, get_score())
+    #exit condition
+    if cnt == n:
+        return
+
+    #normal
+    for player in range(1, k+1): #플레이어 중 한명선택
+        if positions[player] >= m:
+            continue
+        positions[player] += distances[cnt] #현재 턴에서 갈 수 있는 거리 더하기
+        choose(cnt+1)
+        positions[player] -= distances[cnt]        
+    return
+
+choose(0)
+print(result)
+    
+    #for dis in distances:
+    #    positions[player] += dis
+    #    choose(cnt + 1, )
