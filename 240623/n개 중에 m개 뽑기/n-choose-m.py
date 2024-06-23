61af7a8a045f57c8f553800717bdf54db90c0439
heescodes <105621606+heescodes@users.noreply.github.com>
2024-06-23 16:18:53 +0900

    Title: n개 중에 m개 뽑기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240623/n개 중에 m개 뽑기/n-choose-m.py
+++ 240623/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,32 +1,22 @@
-# 변수 선언 및 입력
+n,m = map(int, input().split())
+result = []
 
-n, m = tuple(map(int, input().split()))
-combination = []
+def print_res():
+    print(" ".join(map(str, result)))
+    return
 
-
-# 방문한 원소들을 출력해줍니다.
-def print_combination():
-    for elem in combination:
-        print(elem, end = " ")
-    
-    print()
-
-
-def find_combination(curr_num, cnt):
-    # n개의 숫자를 모두 탐색했으면 더 이상 탐색하지 않습니다.
-    if curr_num == n + 1:
-        # 탐색하는 과정에서 m개의 숫자를 뽑은 경우 답을 출력해줍니다.
-        if cnt == m:
-            print_combination()
+def choose(cnt):
+    #exit condition
+    if len(result) == m:
+        print_res()
         return
 
-    # curr_num에 해당하는 숫자를 사용했을 때의 경우를 탐색합니다.
-    combination.append(curr_num)
-    find_combination(curr_num + 1, cnt + 1)
-    combination.pop()
+    #normal
+    for x in range(cnt, n+1):
+        result.append(x)
+        #print("x,result,len(): %d,%s,%d" % (x,result,len(result)))
+        choose(x+1)
+        result.pop()
+    return
 
-    # curr_num에 해당하는 숫자를 사용하지 않았을 때의 경우를 탐색합니다.
-    find_combination(curr_num + 1, cnt)
-
-
-find_combination(1, 0)
+choose(1)
