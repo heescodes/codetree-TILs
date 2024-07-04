1ac98aa23e7240acf5631f4d3fa67cd88af93998
heescodes <105621606+heescodes@users.noreply.github.com>
2024-07-05 00:55:15 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 2173ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240705/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240705/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -8,12 +8,13 @@ arr = list(list(map(int, input().split())) for _ in range(n))
 dx = [+1, -1, 0, 0] 
 dy = [0, 0, +1, -1]
 q = deque()
+visited = list([False]*m for _ in range(n))
 
 def init_pos():
     q.append((0,0))
 
 def is_valid(x, y):
-    if 0<=x<=m-1 and 0<=y<=n-1 and not(arr[y][x]):
+    if 0<=x<=m-1 and 0<=y<=n-1 and arr[y][x] == 1:
         return True
     else:
         return False
@@ -27,17 +28,21 @@ def is_escaped(x, y):
     if (y == n-1) and (x == m-1):
         return True
     else:
-        False
+        return False
 
 def bfs():
     init_pos()
     while q:
+        #print(f'q:{q}')
         x, y = q.popleft()
+        visited[y][x] = True
         for i in range(_4WAY):
             nx, ny = mov(i, x, y)
-            if is_valid(nx,ny):
-                #print(f'nx,ny:{nx},{ny}')
-                q.append((nx,ny))
+            #print(f'(nx,ny):({nx},{ny})')
+            if is_valid(nx,ny) and not(visited[ny][nx]):
+                q.append((nx, ny))
+            else:
+                continue
             if is_escaped(nx,ny):
                 return 1  
     return 0
