327a0a31da3868e7c687627deedfa9b86a9f63b5
heescodes <105621606+heescodes@users.noreply.github.com>
2024-07-07 18:43:18 +0900

    Title: 최소 경로로 탈출 하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240707/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 240707/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -1,45 +1,36 @@
 from collections import deque
-n,m = map(int, input().split())
+
+n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-result = n*m
-visited = list([False]*m for _ in range(n))
-q = deque()
-dx = [+1,-1,0,0]
-dy = [0,0,+1,-1]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-def in_range(x,y):
-    return 0<=x<=m-1 and 0<=y<=n-1
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
 
-def is_snake(x,y):
-    return arr[y][x]==0
+def is_snake(x, y):
+    return arr[x][y] == 0
 
-def is_arrived(x,y):
-    return (x == m-1) and (y == n-1)
+def get_minlen():
+    q = deque([(0, 0)])
+    visited = [[False] * m for _ in range(n)]
+    cnt = 0
+    visited[0][0] = True
 
-def move(dirnum,x,y):
-    nx = x+dx[dirnum]
-    ny = y+dy[dirnum]
-    return nx,ny
-
-def get_minlen(sx,sy):
-    lcnt=0
-    q.append((sx,sy))
-    visited[sy][sx] = True
     while q:
         x, y = q.popleft()
-        if is_arrived(x,y):
-            return lcnt
+        if x == n-1 and y == m-1:
+            return distance[x][y]
 
         for i in range(4):
-            nx, ny = move(i,x,y)
-            if in_range(nx,ny) and not(is_snake(nx,ny)) and not(visited[ny][nx]):
-                q.append((nx,ny))
-                visited[ny][nx] = True
-                lcnt += 1
-                print(f'nx,ny=({nx},{ny})')
-                print(f'lcnt={lcnt}')
-                
+            nx, ny = x + dx[i], y + dy[i]
+            if in_range(nx, ny) and not is_snake(nx, ny) and not visited[nx][ny]:
+                q.append((nx, ny))
+                visited[nx][ny] = True
+                distance[nx][ny] = distance[x][y] + 1
+
     return -1
 
-print(get_minlen(0,0))
+result = get_minlen()
+print(result)
