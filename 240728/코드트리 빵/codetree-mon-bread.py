6bcaed51b93aef980fa763d7854ddd80fbfb6ae4
heescodes <105621606+heescodes@users.noreply.github.com>
2024-07-28 17:54:43 +0900

    Title: 코드트리 빵, Time: 5061ms, Memory: 146MB, Status: Time Limit Exceed - CodeTree

--- 240728/코드트리 빵/codetree-mon-bread.py
+++ 240728/코드트리 빵/codetree-mon-bread.py
@@ -1,9 +1,9 @@
 from collections import deque
 
 ### Input ###
-n,m = map(int, input().split())
-arr = [[None]*(n+1) for _ in range(n+1)]
-for i in range(1, n+1):
+n, m = map(int, input().split())
+arr = [[None] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
     arr[i][1:] = list(map(int, input().split()))
 stores = [None] + [tuple(map(int, input().split())) for _ in range(m)]
 ##############
@@ -11,8 +11,8 @@ stores = [None] + [tuple(map(int, input().split())) for _ in range(m)]
 ### Global ###
 dx = [0, -1, +1, 0]
 dy = [-1, 0, 0, +1]
-player_cpos = [None]*(m+1)
-player_visited = [None] + [list([False]*(n+1) for _ in range(n+1)) for _ in range(m)]
+player_cpos = [None] * (m + 1)
+player_visited = [None] + [list([False] * (n + 1) for _ in range(n + 1)) for _ in range(m)]
 ban_list = list()
 done_cnt = 0
 ##############
@@ -40,7 +40,7 @@ def set_ban(pair):
 def get_base(player):  # BFS 기본포맷 익숙해지기
     found_base = []
     dq = deque()
-    visited = [[False]*(n+1) for _ in range(n+1)]
+    visited = [[False] * (n + 1) for _ in range(n + 1)]
     x, y = stores[player]
     dq.append((x, y))
     visited[x][y] = True
@@ -65,7 +65,8 @@ def get_min_path(player, cpos):
     x, y = cpos
     
     dq.append(cpos)
-    player_visited[player][x][y] = True
+    visited = [[False] * (n + 1) for _ in range(n + 1)]  # BFS용 visited 배열
+    visited[x][y] = True
     parent = {(x, y): None}
 
     while dq:
@@ -73,10 +74,10 @@ def get_min_path(player, cpos):
         for i, j in zip(dx, dy):
             nx, ny = x + i, y + j
             if cpos == (2, 2):
-                print(f'{nx, ny}의 visit여부: {player_visited[player][nx][ny]}')
-            if can_go((nx, ny)) and not player_visited[player][nx][ny]:
+                print(f'{nx, ny}의 visit여부: {visited[nx][ny]}')
+            if can_go((nx, ny)) and not visited[nx][ny]:
                 dq.append((nx, ny))
-                player_visited[player][nx][ny] = True
+                visited[nx][ny] = True
                 parent[(nx, ny)] = (x, y)
 
                 if (nx, ny) == stores[player]:
@@ -132,7 +133,7 @@ def do_action(player, t):
 time = 1
 while done_cnt < m:
     print(f'----------time: {time}----------')
-    for player in range(1, m+1):
+    for player in range(1, m + 1):
         print(f'done_cnt = {done_cnt}')
         do_action(player, time)
     do_ban()
